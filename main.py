c9d9c9648f1437fec162acd58ffa62046d311bd6
JOHNMARK <hoomanthe3rd@gmail.com>
2024-10-28 06:00:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
